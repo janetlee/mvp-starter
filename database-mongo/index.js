6d6ce57c9762cbbd425fc19e17fe56c4ee052c35
Janet Lee <janetjhu96@gmail.com>
2017-11-10 17:21:30 -0800

    Rename helpersNWS.js file to helpers.js, finalize NWS data returned, add XML2JS package

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mvpproject');
+mongoose.connect('mongodb://localhost/mvpproject', {useMongoClient: true});
 
 var db = mongoose.connection;
 
@@ -11,19 +11,51 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
-var itemSchema = mongoose.Schema({
-  zipcode: { type: String, required: true},
-  timeStart: { type: Date, required: true},
-  timeEnd: { type: Date, required: true},
-  tempMax: { type: Number, required: true},
-  tempMin: { type: Number, required: true}
+let weatherSchema = mongoose.Schema({
+  lat: { type: String, required: true},
+  long: { type: String, required: true},
+  timeStart: { type: String, required: true},
+  timeEnd: { type: String, required: true},
+  tempType: { type: Number, required: true},
+  temp: { type: Number, required: true}
   // TODO: CREATE UNIQUE COMPOSITE INDEX
 });
 
-var Item = mongoose.model('Item', itemSchema);
+let geocodeSchema = mongoose.Schema({
+  zipcode: { type: String, required: true},
+  lat: { type: String, required: true},
+  long: { type: String, required: true}
+  // TODO: CREATE UNIQUE COMPOSITE INDEX
+});
 
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
+let Weather = mongoose.model('Weather', weatherSchema);
+let Geocode = mongoose.model('Geocode', geocodeSchema);
+
+var saveWeather = ((data) => {
+
+  var incomingData = {
+    lat: result.dwml.data[0].location[0].point[0]['$']['latitude'],
+    long: result.dwml.data[0].location[0].point[0]['$']['longitude'],
+    timeStart: result.dwml.data[0]['time-layout'][0]['start-valid-time'], // begin
+    timeEnd: result.dwml.data[0]['time-layout'][0]['end-valid-time'], //end
+    tempType: result.dwml.data[0].parameters[0].temperature[0].name,
+    temp: result.dwml.data[0].parameters[0].temperature[0].value
+  };
+
+  var weather = new Weather(incomingData);
+  weather.save()
+  .then(data => {
+    console.log("Weather saved to database");
+  })
+  .catch(err => {
+    console.log(err);
+    console.log("unable to save to database");
+  });
+});
+
+
+var retrieveWeather = function(callback) {
+  Weather.find({}, function(err, items) {
     if(err) {
       callback(err, null);
     } else {
@@ -32,4 +64,5 @@ var selectAll = function(callback) {
   });
 };
 
-module.exports.selectAll = selectAll;
+module.exports.saveWeather = saveWeather;
+module.exports.retrieveWeather = retrieveWeather;
---
+++ helpers/helpers.js
@@ -0,0 +1,57 @@
+// National Weather Service helpers
+var config = require('../config');
+var express = require('express');
+var Promises = require('bluebird');
+var request = require('request');
+
+module.exports.getNWSData = function(body) {
+  let options = {
+    url: 'https://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php?zipCodeList='
+        + body.zipcode
+// TODO: Need to parse for date things to enter today's date
+        + '&product=time-series&begin=2017-10-10T00:00:00&end=2017-11-11T00:00:00&maxt=maxt&mint=mint',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${config.NWSTOKEN}`
+    }
+  };
+
+  console.log(options);
+  // pass the requst options to the request in a Promise
+  return new Promise ((resolve, reject) => {
+    request(options, (err, res, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Inside the promise');
+        resolve(body);
+      }
+    })
+  })
+};
+
+//https://maps.googleapis.com/maps/api/geocode/json?address=21218&key=
+module.exports.getGeocoding = function(body) {
+  let options = {
+    url: 'https://maps.googleapis.com/maps/api/geocode/json',
+    method: 'GET',
+    address: body.zipcode,
+    key: config.GoogleMAPS_API_KEY,
+    headers: {
+      'User-Agent': 'request',
+    }
+  };
+
+  console.log(options);
+  // pass the requst options to the request in a Promise
+  return new Promise ((resolve, reject) => {
+    request(options, (err, res, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Inside the promise');
+        resolve(body);
+      }
+    })
+  })
+};
