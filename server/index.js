d03724895fb1ab0d7678a306ef150e787b3f4413
Janet Lee <janetjhu96@gmail.com>
2017-11-11 17:13:27 -0800

    Fix data retrieval promises, rework DB schema to one table, combine GoogleMaps and NWS API payloads to single data object

--- server/index.js
+++ server/index.js
@@ -3,20 +3,24 @@ var bodyParser = require('body-parser');
 var items = require('../database-mongo');
 var helpers = require('../helpers/helpers.js');
 var XMLParser = require('xml2js').parseString;
+var underscore = require('underscore');
+var Promises = require('bluebird');
+
+Promises.promisify(XMLParser);
 
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded()); // text/xml
+app.use(bodyParser.urlencoded());
 
 
-// Internal get call
 app.get('/items', function (req, res) {
-  items.retrieveWeather(function(err, data) {
+  items.retrieveWeather(req.body, function(err, data) {
     if(res === undefined) {
       res.sendStatus(500);
     } else {
+      console.log('WEATHER DATA INSIDE GET CALL', data);
       res.json(data);
     }
   });
@@ -26,35 +30,65 @@ app.post('/items', ((req, res, next) => {
   if (!req) {
     res.status(500).send('Bad request');
   } else {
-    console.log('Received POST call data: ', req.body);
-
+    console.log('Received POST on Server', req.body);
     helpers.getGeocoding(req.body)
       .then((body) => {
-        console.log('Sending Geocode to DB');
-        items.saveGeocode(body);
-        console.log('Returning from Geocode DB');
+        geocodeBody = body;
+
+        helpers.getNWSData(req.body)
+          .then((body) => {
+            return new Promise ((resolve, reject) => {
+              XMLParser(body, function (err, result) {
+                if (err) {
+                  reject(err);
+                } else {
+                  console.log('INSIDE THE PARSER');
+                  weatherBody = result;
+                  // underscore.extend(result, geocodeBody);
+                  resolve(underscore.extend(result, geocodeBody));
+                }
+              })
+            })
+          })
+          .then((result) => {
+            console.log('PARSED TEXT TO PASS TO SAVE FUNCTION', result);
+            return new Promise ((resolve, reject) => {
+              items.saveWeather(result, function (err, zipcode) {
+                if (err) {
+                  reject({message: err, location: 'Dying in saveWeather invocation'});
+                } else {
+                  console.log(result.results[0]['address_components'][0]['long_name']);
+                  resolve(result.results[0]['address_components'][0]['long_name']);
+                }
+              })
+            })
+          })
+          .then((zipcode) => {
+            console.log('LOGGING INCOMING ZIP BEFORE RETRIEVAL', zipcode);
+            return new Promise ((resolve, reject) => {
+              items.retrieveWeather(zipcode, function (err, data) {
+                if (err) {
+                  reject({message: err, location: 'Dying in retrieveWeather invocation'});
+                } else {
+                  console.log('RETRIEVAL DATA', data);
+                  resolve(data);
+                }
+              })
+            })
+          })
+          .then((data) => {
+            console.log('DATA BEFORE SENDING OUT',  data);
+            res.status(201).send(data);
+          })
+        .catch((err) => {
+          console.log(err.message, err.location);
+          console.log('handling promise rejection on NWS data write');
+        });
       })
       .catch((err) => {
         console.log(err);
         console.log('handling promise rejection on Geocode data write');
       });
-
-// TEMPORARILY DISABLING NWS CALLS
-    helpers.getNWSData(req.body)
-      .then((body) => {
-        console.log('NWS data received');
-        // setTimeout(function() {
-        XMLParser(body, function (err, result) {
-          console.log('INSIDE THE PARSER');
-          items.saveWeather(result);
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log('handling promise rejection on NWS data write');
-      });
-
-    res.status(201).send('Received POST');
   }
 }));
 
